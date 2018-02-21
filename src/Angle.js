a0bef538632a64a81d492df634c4e7caa206ec9a
Tiago Mergulhão <me@tmergulhao.com>
2018-02-21 15:14:59 -0300

    Simplification is never enough

--- src/Angle.js
+++ src/Angle.js
@@ -1,21 +1,4 @@
-const CompressionRatio = {
-    best: {
-      selectionLabel: "Best",
-      ratio: 1.0
-    },
-    better: {
-      selectionLabel: "Better",
-      ratio: 0.9
-    },
-    good: {
-      selectionLabel: "Good",
-      ratio: 0.8
-    },
-    average: {
-      selectionLabel: "Average",
-      ratio: 0.7
-    }
-}
+const CompressionRatio = require('./CompressionRatio');
 
 class Angle {
 
